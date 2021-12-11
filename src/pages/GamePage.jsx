4d1d2c25880e5da480d169150321be1cfde6a41c
Monakhrsn <Monakhrsn@gmail.com>
2021-12-11 20:59:05 +0100

    player plays on its turn

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -6,21 +6,36 @@ const GamePage = () => {
   const column = 7;
 
   const [board, setBoard] = useState(generateBoard(row, column));
+  const [currentTurn, setCurrentTurn] = useState('red');
 
-  const clickHandler = (i) => {
-    console.log('You have clicked on me' + i);
+  const clickHandler = (column) => {
+    const newBoard = [...board];
+    for (let i = 1; i <= row; i++) {
+      if (newBoard[row - i][column] === null) {
+        newBoard[row - i][column] = currentTurn;
+        if (currentTurn === 'red') {
+          setCurrentTurn('yellow');
+        } else {
+          setCurrentTurn('red');
+        }
+        break;
+      }
+    }
+
+    setBoard(newBoard);
   };
 
   return (
     <div>
-      <h1>here the game displays</h1>
-      {board.map((row, i) => (
-        <div key={`row${i}`} className='flex board'>
-          {row.map((cell, j) => (
+      <h1>It is {currentTurn}'s turn </h1>
+
+      {board.map((row, rowNumber) => (
+        <div key={`row${rowNumber}`} className='flex board'>
+          {row.map((cell, columnNumber) => (
             <div
-              key={`column${j}`}
-              className='nullCell'
-              onClick={() => clickHandler(j)}
+              key={`column${columnNumber}`}
+              className={cell ? cell : 'nullCell'}
+              onClick={() => clickHandler(columnNumber)}
             ></div>
           ))}
         </div>
