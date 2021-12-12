7e3bbdd12d8148dfeba60a4875d03c967ba81dea
Monakhrsn <Monakhrsn@gmail.com>
2021-12-12 21:17:36 +0100

    Front end completed

--- src/components/helpers.jsx
+++ src/components/helpers.jsx
@@ -1,4 +1,4 @@
-export function generateBoard(row, column) {
+const generateBoard = (row, column) => {
   const board = [];
 
   for (let i = 0; i < row; i++) {
@@ -8,4 +8,142 @@ export function generateBoard(row, column) {
   }
 
   return board;
-}
+};
+
+const isPlayerWinner = (board, playedCell) => {
+  if (isVerticallyWin(board, playedCell)) return true;
+  if (isHorizontallyWin(board, playedCell)) return true;
+  if (isTopLeftToBottomRightDiagonallyWin(board, playedCell)) return true;
+  if (isTopRightToBottomLeftDiagonallyWin(board, playedCell)) return true;
+
+  return false;
+};
+
+const isVerticallyWin = (board, playedCell) => {
+  let counter = 1;
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.row + i === board.length) return false;
+
+    if (
+      board[playedCell.row + i][playedCell.column] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else return false;
+  }
+};
+
+const isHorizontallyWin = (board, playedCell) => {
+  let counter = 1;
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.column + i === board[0].length) break;
+
+    if (
+      board[playedCell.row][playedCell.column + i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.column - i === -1) return false;
+
+    if (
+      board[playedCell.row][playedCell.column - i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+const isTopLeftToBottomRightDiagonallyWin = (board, playedCell) => {
+  let counter = 1;
+  for (let i = 1; i <= 3; i++) {
+    if (
+      playedCell.column + i === board[0].length ||
+      playedCell.row + i === board.length
+    )
+      break;
+
+    if (
+      board[playedCell.row + i][playedCell.column + i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.column - i === -1 || playedCell.row - i === -1) return false;
+
+    if (
+      board[playedCell.row - i][playedCell.column - i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+const isTopRightToBottomLeftDiagonallyWin = (board, playedCell) => {
+  let counter = 1;
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.column + i === board[0].length || playedCell.row - i === -1)
+      break;
+
+    if (
+      board[playedCell.row - i][playedCell.column + i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      break;
+    }
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    if (playedCell.column - i === -1 || playedCell.row + i === board.length)
+      return false;
+
+    if (
+      board[playedCell.row + i][playedCell.column - i] ===
+      board[playedCell.row][[playedCell.column]]
+    ) {
+      counter++;
+
+      if (counter === 4) return true;
+    } else {
+      return false;
+    }
+  }
+
+  return false;
+};
+
+export { isPlayerWinner, generateBoard };
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { generateBoard } from '../components/helpers';
+import { generateBoard, isPlayerWinner } from '../components/helpers';
 
 const GamePage = () => {
   const row = 6;
@@ -7,12 +7,22 @@ const GamePage = () => {
 
   const [board, setBoard] = useState(generateBoard(row, column));
   const [currentTurn, setCurrentTurn] = useState('red');
+  const [isWinner, setIsWinner] = useState(false);
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
   const clickHandler = (column) => {
+    if (isWinner) return;
+
     const newBoard = [...board];
     for (let i = 1; i <= row; i++) {
       if (newBoard[row - i][column] === null) {
         newBoard[row - i][column] = currentTurn;
+
+        if (isPlayerWinner(board, { row: row - i, column: column })) {
+          setIsWinner(true);
+          return;
+        }
+
         if (currentTurn === 'red') {
           setCurrentTurn('yellow');
         } else {
@@ -25,22 +35,45 @@ const GamePage = () => {
     setBoard(newBoard);
   };
 
+  const resetGame = () => {
+    setBoard(generateBoard(row, column));
+    setCurrentTurn('red');
+    setIsWinner(false);
+  };
+
+  const startGame = () => {
+    setIsGameStarted(true);
+  }
+
+
   return (
     <div>
-      <h1>It is {currentTurn}'s turn </h1>
+    {
+      isGameStarted ? <div>
+          <h2>{isWinner ? `${currentTurn} won! 🎉 `: `▶️ Turn to ${currentTurn}`}</h2>
+          <div className='wrapper'>
+            {board.map((row, rowNumber) => (
+              <div key={`row${rowNumber}`} className='flex board'>
+                {row.map((cell, columnNumber) => (
+                  <div
+                    key={`column${columnNumber}`}
+                    className={cell ? cell : 'nullCell'}
+                    onClick={() => clickHandler(columnNumber)}
+                  ></div>
+                ))}
+              </div>
+            ))}
+          </div>
 
-      {board.map((row, rowNumber) => (
-        <div key={`row${rowNumber}`} className='flex board'>
-          {row.map((cell, columnNumber) => (
-            <div
-              key={`column${columnNumber}`}
-              className={cell ? cell : 'nullCell'}
-              onClick={() => clickHandler(columnNumber)}
-            ></div>
-          ))}
+          <button onClick={() => resetGame()}>Reset the game</button>
         </div>
-      ))}
-    </div>
+        :
+        <div>
+          <h1 className="mainHeader">Welcome to Connct-4</h1>
+          <button onClick={startGame}>start</button>
+        </div>
+    }
+        </div>
   );
 };
 
